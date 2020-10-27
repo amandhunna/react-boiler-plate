c1a8306854309367ae9ddd84c71dc7f2a0e06a56
amanjot.singh <amanjot.singh@netmeds.com>
2020-10-27 19:08:01 +0530

    updated webpack config 1.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    plugins: [ new HtmlWebpackPlugin({
-        title: "Application name",
-        template: './public/index.html'
-    })],
-    devServer: {
-        contentBase: path.resolve(__dirname, "./src"),
-        historyApiFallback: true
-    },
-    entry: path.resolve(__dirname, "./src/index.js"),
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: "bundle.js"
-    },
-    module: {
-      rules: [ {
+const commonModule = {
+  module: {
+    noParse: (content) => /.tff/.test(content),
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test:/\.(css|scss)$/,
+        use:["style-loader", "css-loader"]
+      },
+      {
         test: /\.(jpe?g|png|svg|gif)$/,
         // use:["url-loader"] or
         use: [{
@@ -26,23 +26,42 @@ module.exports = {
           }
         }]
       },
-        {
-          test: /\.html$/i,
-          loader: 'html-loader',
-          options: {
-            // Disables attributes processing
-            attributes: false,
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+  ]
+ }
+}
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    ...commonModule,
+      plugins: [ 
+        new HtmlWebpackPlugin({
+        title: "Kivi Health",
+        template: './public/index.html'
+    })],
+    devServer: {
+        contentBase: path.resolve(__dirname, "./dist"),
+        historyApiFallback: true
+    },
+    entry: path.resolve(__dirname, "./src/index.js"),
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: '[name].[hash].js',
+    },
+      optimization: {
+        // https://webpack.js.org/guides/caching/#output-filenames
+        moduleIds: 'hashed',
+        runtimeChunk: 'single',
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'node_modules',
+              chunks: 'all',
+            },
           },
         },
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            test: /\.js$/i,
-            exclude: /node_modules/,
-            use: ['babel-loader'],
-          },
-      ],
-    },
-  };
+      },
+    
+};
